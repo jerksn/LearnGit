f9840d17f40098afd9607068ec6e306df3855489
Georg Dubrow <geodub83+github@gmail.com>
2023-06-23 22:00:46 +0200

    my typescript

---
+++ TS/practice.js
@@ -0,0 +1,8 @@
+console.log("Hello World");
+var myHeading = document.querySelector("h2");
+var myVar;
+myVar = "Hello TS orld";
+if (myHeading !== null) {
+    myHeading.textContent = myHeading.textContent + " !  " + myVar;
+}
+console.log(myVar);
---
+++ TS/practice.ts
@@ -0,0 +1,13 @@
+console.log("Hello World");
+
+const myHeading = document.querySelector("h2");
+
+let myVar: string;
+myVar = "Hello TS orld";
+
+
+if (myHeading !== null) {
+    myHeading.textContent = myHeading.textContent + " !  " + myVar;
+}
+
+console.log(myVar);
