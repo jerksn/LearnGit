d210abbc4993df9639b1d5943348bfe9148df192
Georg Dubrow <geodub83+github@gmail.com>
2023-06-25 10:54:47 +0200

    my branch python befor merge

--- typeton.py
+++ typeton.py
@@ -1,3 +1,20 @@
-stringvar: string = "thisisastring"
+stringvar: str = "thisisastring"
 
 print(stringvar)
+print(type(stringvar))
+
+stringvar = 1
+
+print(stringvar)
+print(type(stringvar))
+
+intvar: int = 2
+
+print(intvar)
+print(type(intvar))
+
+intvar: int = "string 3"
+
+print(intvar)
+print(type(intvar))
+
