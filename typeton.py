baa6d8af14495ee2d95f8834a4bf2ddaee6d2415
Georg Dubrow <geodub83+github@gmail.com>
2023-06-29 23:25:38 +0200

    toying with fibs

--- typeton.py
+++ typeton.py
@@ -18,4 +18,11 @@ intvar: int = "string 3"
 print(intvar)
 print(type(intvar))
 
-print("featrue branch")
+
+def fib(n):
+    if n <= 1:
+        return n
+    else:
+        return fib(n-1)+fib(n-2)
+    
+print(fib(10))
